3cc8c2fd0dd8657445fd99741472abc991aedfad
Pavel Glukhov <pvglukhov@gmail.com>
2025-05-08 14:37:04 +0300

    [main] добавил код для выполнения урока 2

---
+++ src/main/java/ru/job4j/kafka/consumer/TopicMessagesConsumerV2.java
@@ -0,0 +1,42 @@
+package ru.job4j.kafka.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+/**
+ * Консьюмер для чтения из Топика message
+ * Сделал одну группу, что-бы читали из разных партиций
+ *
+ * @author Павел Глухов
+ */
+public class TopicMessagesConsumerV2 {
+    private static final Logger logger = LoggerFactory.getLogger(TopicMessagesConsumerV2.class);
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("group.id", "demo-consumer-group-1");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("auto.offset.reset", "earliest"); // Чтение с начала
+
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            consumer.subscribe(Collections.singletonList("message"));
+
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+                for (ConsumerRecord<String, String> record : records) {
+                    logger.info("Received message: key = {}, value = {}, partition = {}, offset = {}",
+                            record.key(), record.value(), record.partition(), record.offset());
+                }
+            }
+        }
+    }
+}
