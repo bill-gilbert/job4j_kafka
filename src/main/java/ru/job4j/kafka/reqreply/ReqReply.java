798274afbc50ee017836d0a401b8eaed40452cf2
Pavel Glukhov <pvglukhov@gmail.com>
2025-05-11 22:52:48 +0300

    [main] добавил код для выполнения урока 4

---
+++ src/main/java/ru/job4j/kafka/reqreply/ReqReply.java
@@ -0,0 +1,71 @@
+package ru.job4j.kafka.reqreply;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
+/**
+ * Имплементация паттерна Request/Reply
+ * Переконвертил с Kotlin
+ *
+ *
+ */
+public class ReqReply {
+    private static final Logger logger = LoggerFactory.getLogger(ReqReply.class);
+
+    private final long timeout;
+    private final Object monitor = new Object();
+    private String message = "";
+    private boolean received = false;
+
+    public ReqReply(long timeout) {
+        this.timeout = timeout;
+    }
+
+    public String send() {
+        synchronized (monitor) {
+            if (!received) {
+                try {
+                    monitor.wait(timeout);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    return "Interrupted while waiting";
+                }
+            }
+            return received ? message : "Happened timeout " + timeout;
+        }
+    }
+
+    public void receive(String text) {
+        synchronized (monitor) {
+            received = true;
+            message = text;
+            monitor.notifyAll();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        ReqReply reply = new ReqReply(10);
+        FutureTask<String> task = new FutureTask<>(new Callable<>() {
+            @Override
+            public String call() {
+                return reply.send();
+            }
+        });
+
+        new Thread(task).start();
+
+        new Thread(() -> {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            reply.receive("Send message");
+        }).start();
+
+        logger.info(task.get());
+    }
+}
